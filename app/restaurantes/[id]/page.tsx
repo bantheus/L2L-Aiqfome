c4571605369d0f293a89139a70ee1f2c0eeeab2b
bantheus <matheuschmidt.b@gmail.com>
2025-06-21 15:40:10 -0300

    refactor (restaurant-page): refactor metadata generation

--- app/restaurantes/[id]/page.tsx
+++ app/restaurantes/[id]/page.tsx
@@ -1,15 +1,45 @@
 import NotFound from "@/components/not-found";
 import { getRestaurantById } from "@/mock/mock";
+import type { Metadata } from "next";
 import RestaurantInfos from "./components/restaurant-infos";
 import RestaurantMenu from "./components/restaurant-menu";
 import RestaurantTitle from "./components/restaurant-title";
 
-export default async function RestaurantPage({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
+type Params = Promise<{ id: string }>;
+
+export async function generateMetadata(props: {
+  params: Params;
+}): Promise<Metadata> {
+  const params = props.params;
   const { id } = await params;
+  const restaurantData = getRestaurantById(id);
+
+  if (!restaurantData) {
+    return {
+      title: "Restaurante não encontrado",
+      description: "O restaurante que você está procurando não existe.",
+    };
+  }
+
+  return {
+    title: restaurantData.name,
+    description: `Cardápio do restaurante ${restaurantData.name}`,
+    openGraph: {
+      title: restaurantData.name,
+      description: `Cardápio do restaurante ${restaurantData.name}`,
+      images: [
+        {
+          url: restaurantData.logoUrl,
+          alt: restaurantData.name,
+        },
+      ],
+    },
+  };
+}
+
+export default async function RestaurantPage(props: { params: Params }) {
+  const params = props.params;
+  const id = (await params).id;
   const restaurantData = getRestaurantById(id);
 
   if (!restaurantData) {
